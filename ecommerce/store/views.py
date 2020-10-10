ea7c1b2f99e5405c14d8e1b93d35e41cb2b5d482
RoxanneG93 <rgarcia@vnnsports.net>
2020-10-10 15:46:33 -0500

    adding cart and checkout data

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -12,10 +12,31 @@ def store(request):
 
 def cart(request):
     template = 'store/cart.html'
-    context = {}
+    #items = []
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # look more on how these two queries works
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # guest empty cart for non logged-in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items':items, 'order': order}
     return render(request, template, context)
 
 def checkout(request):
     template = 'store/checkout.html'
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # look more on how these two queries works
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # guest empty cart for non logged-in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items':items, 'order': order}
     return render(request, template, context)
